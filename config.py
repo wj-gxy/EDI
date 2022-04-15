1b384a97c2139672e666d0f9381565cd9c87f6c5
wj-gxy <61655426+wj-gxy@users.noreply.github.com>
2022-04-15 12:19:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,28 +12,17 @@ class Config:
     study_file = 'dataset/visiont/test_10.json'
     saved_model = 'model/best_model.pt'
     device = torch.device("cuda:0")
-    # device = torch.device("cpu")
     train_epochs = 200
     batch_size = 32
-    learning_rate = 0.001
+    learning_rate = 0.001  #or 0.005
     learning_rate_decay = 0.99
     l2_regularization = 0.001
     content_count = 1  # max count of content
-    content_length = 32  # max count of content words
+    content_length = 32  # max count of news content words
     comment_count = 10  # max count of user comments
-    review_length = 32  # max count of review words
+    review_length = 32  # max count of comment words
     lowest_review_count = 3  # Minimum number of comments for users to keep
     PAD_WORD = '<UNK>'
     require_improvment = 1000
-    #class_list = ['fake', 'real']   # 微博的2分类
-    class_list = ['false', 'true']  # 推特15,16的2分类
-
-    def __init__(self):
-        attributes = inspect.getmembers(self, lambda a: not inspect.isfunction(a))
-        attributes = list(filter(lambda x: not x[0].startswith('__'), attributes))
-
-        parser = argparse.ArgumentParser()
-        for key, val in attributes:
-            parser.add_argument('--' + key, dest=key, type=type(val), default=val)
-        for key, val in parser.parse_args().__dict__.items():
-            self.__setattr__(key, val)
+    #class_list = ['fake', 'real']   # weibo
+    class_list = ['false', 'true']  # twitter
